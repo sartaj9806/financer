7ec21753a87b488ff5b6c98c3fecc39b1814fac5
Sartaj Alam <Sartaj9806@gmail.com>
2025-03-03 08:46:24 +0530

    maint fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 import { AppContextProvider } from './Context/AppContext.jsx';
-import { AdminContextProvider } from './Context/AdminContex.jsx';
+import { AdminContextProvider } from './Context/AdminContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
